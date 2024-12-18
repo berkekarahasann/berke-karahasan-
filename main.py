9b7eff8d72d47d85ca7db21ea52d9d7844e216e9
berkekarahasann <berke.karahasan@std.yeditepe.edu.tr>
2024-12-18 17:40:53 +0300

    phyton deneme

---
+++ main.py
@@ -0,0 +1,16 @@
+#Basit Hesap Makinesi
+
+print("Basit Hesap Makinesine Hoş Geldiniz")
+
+a = float(input("10 "))
+b = float(input("20"))
+
+toplama = a + b
+cikarma = a - b
+carpma = a * b
+bolme = a / b
+
+print("Toplama sonucu: ", toplama)
+print("cıkarma sonucu: ", cikarma)
+print("carpma sonucu: ", carpma)
+print("Bölme sonucu: ", bolme)
